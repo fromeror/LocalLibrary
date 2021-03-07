6967242e77e778d54c5b1bbf9441ed3260cb0d8f
Fabiola Romero <42563224+fromeror@users.noreply.github.com>
2021-03-06 19:18:30 -0600

    Adding comments

--- public/src/books.js
+++ public/src/books.js
@@ -1,15 +1,18 @@
 // Note: Please do not change the name of the functions. The tests use those names to validate your code.
 
+// Returns author object matching the id 
 function findAuthorById(authors, id) {
   const author = authors.find((author) => author.id === id);
   return author;
 }
 
+// return book object matching the id
 function findBookById(books, id) {
   const book = books.find((book) => book.id === id);
   return book;
 }
 
+// Returns an array with an array or loaned out books and an array with books that have been returned 
 function partitionBooksByBorrowedStatus(books) {
   let result = [];
   const borrowed = books.filter((book) => book.borrows[0].returned === false);
@@ -20,6 +23,7 @@ function partitionBooksByBorrowedStatus(books) {
   return result;
 }
 
+// Returns an array of 10 or less borrow transactions and account info
 function getBorrowersForBook(book, accounts) {
   let result = book.borrows.map((borrows) => {
     let account = accounts.find((account) => account.id === borrows.id);
